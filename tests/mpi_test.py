c2b05e4b3822e96a63d9a33eaa6517aeaf036ab2
hiphopkorzhik <148270384+hiphopkorzhik@users.noreply.github.com>
2023-12-18 21:21:51 +0300

    Update mpi_test.py
    ept ya ustal

--- tests/mpi_test.py
+++ tests/mpi_test.py
@@ -12,13 +12,13 @@ class ConditionTest(TestCase):
     #     self.assertEqual(u_left(0.1), 11)
 
     def test_4_processes(self):
-        self.assertEqual(os.system("mpiexec --oversubscribe -n 4 python ../dev/mpi_app.py"), 0)
+        self.assertEqual(os.system("mpiexec --oversubscribe -n 4 python ./dev/mpi_app.py"), 0)
 
     def test_6_processes(self):
-        self.assertEqual(os.system("mpiexec --oversubscribe -n 6 python ../dev/mpi_app.py"), 0)
+        self.assertEqual(os.system("mpiexec --oversubscribe -n 6 python ./dev/mpi_app.py"), 0)
 
     def test_8_processes(self):
-        self.assertEqual(os.system("mpiexec --oversubscribe -n 8 python ../dev/mpi_app.py"), 0)
+        self.assertEqual(os.system("mpiexec --oversubscribe -n 8 python ./dev/mpi_app.py"), 0)
 
 if __name__=="__main__":
     main()
